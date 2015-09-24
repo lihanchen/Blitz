5998ffcfab26fe2b8908046058b4536bfc068384
arthurchan35 <arthurchan35@gmail.com>
2015-09-24 15:03:31 -0400

    some

--- Server/Query/query.js
+++ Server/Query/query.js
@@ -1,24 +1,49 @@
 function listen() {
     var port = 5001;
     var net = require('net');
-    var server = net.createServer(function (c) { //'connection' listener
-        console.log('client connected');
-        c.on('close', function () {
-            console.log('client disconnected');
-        });
-        c.on('data', function (data) {
-            var json = JSON.parse(data.toString());
-            check(json.username, json.password);
-        });
-    });
-    server.listen(port, function () { //'listening' listener
-        console.log('server start');
-    });
+    var server = net.createServer(
+        function (c) { //'connection' listener
+            console.log('client connected');
+            c.on('close', 
+                function () {
+                    console.log('client disconnected');
+                }
+            );
+            c.on('data', 
+                function (data) {
+                    var filters = JSON.parse(data.toString());
+                    return query(filters);
+                }
+            );
+        }
+    );
+    server.listen(port, 
+        function () { //'listening' listener
+            console.log('server start');
+        }
+    );
 }
 
-function dosomething() {
+function query(filters) {
+    var  mongodb = require('mongodb');
+    var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+    var  db = new mongodb.Db('490', server);
+    db.open(
+        function(err, db){
+           if(!err){
+                console.log('connect db'); 
+                db.collection('Post',
+                    function(err, collection) {
+                        if(err) {console.log(err);}
+                        else {
+                            collection.find
+                        }
+                    }
+                );
+           }     
+        }
+    );
 }
 
 
 listen();
-//check("lhc1","111");
