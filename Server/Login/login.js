8b50263637e086fc5b2c8b8a832b6285d846f872
Hanchen Li <lihanchenhz@gmail.com>
2015-09-24 19:32:05 -0400

    Error handling Optimize

--- Server/Login/login.js
+++ Server/Login/login.js
@@ -1,47 +1,53 @@
 function listen(){
 	var port=5000;
 	var net = require('net');
-	var server = net.createServer(function(c) { //'connection' listener
-			console.log('client connected');
-			c.on('close', function() {
-				console.log('client disconnected');
-			});
-			c.on('data', function(data) {
-				var json=JSON.parse(data.toString());
-				check(json.username,json.password);
-				c.write("asd\n");
+	var server = net.createServer(function(client) {
+			client.on('data', function(data) {
+				check(data, client);
 			});
 		});
-		server.listen(port, function() { //'listening' listener
-				console.log('server start');
-		});
+		server.listen(port);
 }
 
-function check(username,password){
-	var  mongodb = require('mongodb');
-	var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-	var  db = new mongodb.Db('490', server);
-	db.open(function(err, db){
-			if(!err){
-				console.log('connect db');
-				db.collection('Account', function(err, collection){
-					if(err){
-						console.log(err);
-					}else{
-						collection.findOne({username:username,password:password},function(err,item){
-							if (item!=null){
-								console.log("Success");
-							}else{
-								console.log("Fail");
-							}
-							db.close();
-						}); 
-					}
-				});
+function check(receivedData,socket){
+	var ret={};
+	try{
+		var json=JSON.parse(receivedData.toString());
+		table.findOne({username:json.username},function(err,item){
+			if (item==null){
+				ret.success=false;
+				ret.msg="Username doesn't exist";
+			}else{
+				if (item.password==json.password){
+				ret.success=true;
+				}else{
+				ret.success=false;
+				ret.msg="Password doesn't match";
+				}
 			}
-	});
+			socket.write(JSON.stringify(ret));
+			socket.destroy();
+		}); 
+	}catch(e){
+		console.error(e);
+		ret.error=e.toString();
+		socket.write(JSON.stringify(ret));
+		socket.destroy();
+	}
 }
 
 
-listen();
-//check("lhc1","111");
+var mongodb=require('mongodb');
+var server=new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+var db=new mongodb.Db('490', server);
+var table;
+db.open(function(err, db){
+	if(err){
+		console.log(err);
+	}else{
+		db.collection('Account', function(err, collection){
+			table=collection;
+			listen();
+		});
+	}
+});
