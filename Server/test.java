5ed63dc4187580a1914579543b6edddaf1acfd0b
Hanchen Li <lihanchenhz@gmail.com>
2015-09-24 17:40:37 -0400

    test.java2

---
+++ Server/test.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.io.OutputStreamWriter;
+
+public class test {
+
+	public static void main(String args[]) throws Exception {
+		int port = 5000;
+		String host = "127.0.0.1";
+		final Socket client = new Socket(host, port);
+
+		new Thread(){
+			public void run(){
+				try {
+					BufferedReader screenReader = new BufferedReader(new InputStreamReader(System.in));
+					OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
+					while(true){
+						osw.write(screenReader.readLine());
+						osw.flush();
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}.start();
+
+		BufferedReader responseReader= new BufferedReader(new InputStreamReader(client.getInputStream()));
+		while(true){
+			String response=responseReader.readLine();
+			if (response==null) System.exit(-1);
+			System.out.println(response);
+			System.out.flush();
+		}
+	}
+}
