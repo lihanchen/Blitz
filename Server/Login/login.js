11b50548a5efeab34cf54ef9f57564f39f8b38ad
Hanchen Li <lihanchenhz@gmail.com>
2015-09-24 20:31:56 -0400

    multiple operation

--- Server/Login/login.js
+++ Server/Login/login.js
@@ -1,24 +1,34 @@
 function listen(){
 	var port=5000;
 	var net=require('net');
-	var server=net.createServer(function(client) {
-			client.on('data', function(data) {
-				check(data, client);
+	var server=net.createServer(function(socket) {
+			socket.on('data', function(data) {
+				try{
+					var json=JSON.parse(data.toString());
+					if (json.operation=="Login"){
+						login(json, socket);
+					}
+					socket.write(JSON.stringify({error:"Unknown Operation"}));
+					socket.destroy();
+				}catch(e){
+					console.error(e);
+					socket.write(JSON.stringify({error:e.toString()}));
+					socket.destroy();
+				}
 			});
 		});
 		server.listen(port);
 }
 
-function check(receivedData,socket){
+function login(receivedObj,socket){
 	var ret={};
 	try{
-		var json=JSON.parse(receivedData.toString());
-		table.findOne({username:json.username},function(err,item){
+		table.findOne({username:receivedObj.username},function(err,item){
 			if (item==null){
 				ret.success=false;
 				ret.msg="Username doesn't exist";
 			}else{
-				if (item.password==json.password){
+				if (item.password==receivedObj.password){
 					ret.success=true;
 				}else{
 					ret.success=false;
