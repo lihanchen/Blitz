72dd3829d1da483d2b6926cf858d64d242627eff
Hanchen Li <lihanchenhz@gmail.com>
2015-10-06 15:58:24 -0400

    Query

--- Server/Query/query.js
+++ Server/Query/query.js
@@ -1,52 +1,50 @@
-function listen() {
-    var port = 5001;
-    var net = require('net');
-    var server = net.createServer(function (socket) { //'connection' listener
-            console.log('client connected');
-            socket.on('data', function (data) {
-                try{
-                    console.log('trying');
-                    var json = JSON.parse(data.toString());
-                    query(json, socket);
-                } catch (e) {
-                    console.log('wrong');
-                    console.error(e);
-                    socket.write(JSON.stringify({error:e.toString()}));
-                    socket.destroy();
-                }
-            });
-    });
-    server.listen(port);
+function listen(){
+	var port=5000;
+	var net=require('net');
+	var server=net.createServer(function(socket) {
+			socket.on('data', function(data) {
+				try{
+					var json=JSON.parse(data.toString());
+					if (json.operation=="Query"){
+						query(json, socket);
+					}else{
+						socket.write(JSON.stringify({error:"Unknown Operation"}));
+						socket.destroy();
+					}
+				}catch(e){
+					socket.write(JSON.stringify({error:e.toString()}));
+					socket.destroy();
+				}
+			});
+		});
+		server.listen(port);
 }
+
 
 function query(filters, socket) {
-    var mongodb = require('mongodb');
-    
-    var server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
-    
-    var db = new mongodb.Db('490', server);
-    var res = null;
-    console.log('start querying');
-    db.open(function(err, db){
-       if(!err){
-            console.log('connect db'); 
-            db.collection('posts', function(err, collection) {
-                if(err) {
-                    socket.write(JSON.stringify({error:e.toString()}));
-                    socket.destroy();
-                }
-                else {
-                    res = collection.find(filters);
-                    socket.write(JSON.stringify(ret));
-                    socket.destroy();
-                }
-            });
-       }
-       else {
-        console.log('error processing');
-       }     
-    });
+	delete filters['operation'];
+	global.collection.find(filters).toArray(function(err,doc){
+		socket.write(JSON.stringify(doc));
+		socket.destroy();
+	});
 }
 
 
-listen();
+var mongodb=require('mongodb');
+var server=new mongodb.Server('localhost', 27017, {auto_reconnect:true});
+var db=new mongodb.Db('490', server);
+db.open(function(err, db){
+	if(err){
+		console.log(err);
+	}else{
+		db.collection('Post', function(err, collection){
+			if(err){
+				console.log(err);
+			}else{
+				global.collection=collection;
+				listen();
+			}
+		});
+	}
+});
+
