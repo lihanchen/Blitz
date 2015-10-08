46a3ee9f65f04af7fd2c22c4491ed036a29b4469
Hanchen Li <lihanchenhz@gmail.com>
2015-10-08 00:34:08 -0400

    try catch

--- Server/Http/http.js
+++ Server/Http/http.js
@@ -4,43 +4,53 @@ var url = require('url');
 var mongodb=require('mongodb');
 var server=new mongodb.Server('localhost', 27017, {auto_reconnect:true});
 var db=new mongodb.Db('490', server);
-db.open(function(err, db){
-		if(err){
-			console.log(err);
-		}else{
-			db.collection('Account', function(err, collection){
-				if(err){
-					console.log(err);
-				}else{
-					global.collection=collection;
-					http.createServer(function (request, response) {
-						if (request.method=="POST"){
-							var post='';      
-							var qs=require('querystring');
-							request.on('data',function(chunk){  
-								post += chunk;  
-								if (post.length > 500) request.connection.destroy();
-							});   
-							request.on('end',function(){  
-								var handlerModule=require("./resetHandler");
-								handlerModule.handle(qs.parse(post),response);  
-							});
-						}else{
-							response.writeHead(200, {'Content-Type': 'text/html'});
-							obj=url.parse(request.url,true);
-							if (obj.pathname=="/verify"){
-								var verifyPage=require("./verifyPage");
-								verifyPage.verifyPage(obj,response);
-							}else if (obj.pathname=="/reset"){
-								var resetPage=require("./resetPage");
-								resetPage.resetPage(obj,response);
-							}else{
-								response.end("ERROR");
+try{
+	db.open(function(err, db){
+			if(err){
+				console.log(err);
+			}else{
+				db.collection('Account', function(err, collection){
+					if(err){
+						console.log(err);
+					}else{
+						global.collection=collection;
+						http.createServer(function (request, response) {
+							try{
+								if (request.method=="POST"){
+									var post='';      
+									var qs=require('querystring');
+									request.on('data',function(chunk){  
+										post += chunk;  
+										if (post.length > 500) request.connection.destroy();
+									});   
+									request.on('end',function(){  
+										var handlerModule=require("./resetHandler");
+										handlerModule.handle(qs.parse(post),response);  
+									});
+								}else{
+									response.writeHead(200, {'Content-Type': 'text/html'});
+									obj=url.parse(request.url,true);
+									if (obj.pathname=="/verify"){
+										var verifyPage=require("./verifyPage");
+										verifyPage.verifyPage(obj,response);
+									}else if (obj.pathname=="/reset"){
+										var resetPage=require("./resetPage");
+										resetPage.resetPage(obj,response);
+									}else{
+										response.end("ERROR");
+									}
+								}
+							}catch(e1){
+								request.connection.destroy();
+								console.log(e1);
 							}
-						}
-					}).listen(8000)
-				}
-			});
-		}
-});
+						}).listen(8000)
+					}
+				});
+			}
+	});
+catch(e){
+	console.error(e);
+}
+
 
