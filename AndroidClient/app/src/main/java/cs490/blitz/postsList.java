a0cb8c7bcbdb0cf246406f65f2e7e273f555440d
Hanchen Li <lihanchenhz@gmail.com>
2015-10-31 19:03:54 -0400

    Floating Button

--- AndroidClient/app/src/main/java/cs490/blitz/postsList.java
+++ AndroidClient/app/src/main/java/cs490/blitz/postsList.java
@@ -1,8 +1,10 @@
 package cs490.blitz;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class postsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -10,5 +12,14 @@ public class postsList extends AppCompatActivity {
         setContentView(R.layout.posts_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
