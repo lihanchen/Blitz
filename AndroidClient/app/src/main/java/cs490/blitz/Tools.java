a85ea9446334aa105f5a7cac25f9dcc555f7f84a
Hanchen Li <lihanchenhz@gmail.com>
2015-11-01 10:06:14 -0500

    List Data

--- AndroidClient/app/src/main/java/cs490/blitz/Tools.java
+++ AndroidClient/app/src/main/java/cs490/blitz/Tools.java
@@ -8,9 +8,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public abstract class Tools {
-    public static String query(String queryRequest, int port) {
+    public synchronized static String query(String queryRequest, int port) {
         final String host = "blitzproject.cs.purdue.edu";
         try {
             Socket client = new Socket(host, port);
@@ -25,7 +28,21 @@ public abstract class Tools {
         }
     }
 
-    public static void showToast(Context con, String msg) {
+    public synchronized static void showToast(Context con, String msg) {
         Toast.makeText(con, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public synchronized static String timeProcess(String input) {
+        int year = Integer.parseInt(input.substring(0, 4));
+        int month = Integer.parseInt(input.substring(5, 7));
+        int date = Integer.parseInt(input.substring(8, 10));
+        int hour = Integer.parseInt(input.substring(11, 13));
+        int minute = Integer.parseInt(input.substring(14, 16));
+        int second = Integer.parseInt(input.substring(17, 19));
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+0000"));
+        cal.set(year, month, date, hour, minute, second);
+        SimpleDateFormat format = new SimpleDateFormat("kk:mma MMM dd, yyyy");
+        return format.format(cal.getTime());
     }
 }
--- AndroidClient/app/src/main/java/cs490/blitz/postsList.java
+++ AndroidClient/app/src/main/java/cs490/blitz/postsList.java
@@ -1,12 +1,25 @@
 package cs490.blitz;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class postsList extends AppCompatActivity {
+    static int mode = 0; //0=request 1=offer
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.posts_list);
@@ -18,8 +31,70 @@ public class postsList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //loadData();
             }
         });
+
+        findViewById(R.id.textRequest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mode = 0;
+                findViewById(R.id.textRequest).setBackgroundColor(0xff0003a3);
+                findViewById(R.id.textOffer).setBackgroundColor(0x00000000);
+                loadData();
+            }
+        });
+
+        findViewById(R.id.textOffer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mode = 1;
+                findViewById(R.id.textRequest).setBackgroundColor(0x00000000);
+                findViewById(R.id.textOffer).setBackgroundColor(0xff0003a3);
+                loadData();
+            }
+        });
+
+        loadData();
+    }
+
+    public void loadData() {
+        new AsyncTask<Integer, Integer, JSONArray>() {
+            protected JSONArray doInBackground(Integer... params) {
+                HashMap<String, Object> queryRequest = new HashMap<>();
+                queryRequest.put("operation", "Query");
+                queryRequest.put("isRequest", params[0] == 0);
+                queryRequest.put("TranscationCompleted", false);
+                String ret = Tools.query(JSON.toJSONString(queryRequest), 9067);
+                return JSON.parseArray(ret);
+            }
+
+            protected void onPostExecute(JSONArray jsonArray) {
+                if (jsonArray == null) {
+                    Log.e("Err", "Failed login");
+                    return;
+                }
+                super.onPostExecute(jsonArray);
+
+                ArrayList<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>(jsonArray.size());
+                for (Object obj : jsonArray) {
+                    HashMap<String, Object> map = new HashMap<String, Object>(3);
+                    JSONObject jsonObject = (JSONObject) obj;
+                    if (jsonObject.get("category").equals("FoodDiscover"))
+                        map.put("img", R.drawable.fooddiscover);
+                    else
+                        map.put("img", null);
+                    map.put("title", jsonObject.get("title"));
+                    map.put("time", Tools.timeProcess(jsonObject.get("postTime").toString()));
+                    data.add(map);
+                }
+
+                ListView lv = (ListView) findViewById(R.id.listPostList);
+                SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(), data,
+                        R.layout.list_item, new String[]{"img", "title", "time"},
+                        new int[]{R.id.imageView, R.id.textTitle, R.id.textTime});
+                lv.setAdapter(adapter);
+            }
+        }.execute(mode);
     }
 }
