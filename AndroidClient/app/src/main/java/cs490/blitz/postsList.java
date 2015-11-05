ea223d1f64a584d9d50fd8b296a04349e8b323b0
Dingzhe <cool4074@gmail.com>
2015-11-05 17:03:49 -0500

    Post detail

--- AndroidClient/app/src/main/java/cs490/blitz/postsList.java
+++ AndroidClient/app/src/main/java/cs490/blitz/postsList.java
@@ -110,7 +110,9 @@ public class postsList extends AppCompatActivity {
                 lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                        Log.d("############","id " + id + "position" + position );
+                        Intent postDIntent = new Intent(postsList.this, Postdetail.class);
+                        startActivity(postDIntent);
                     }
                 });
             }
