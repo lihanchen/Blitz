0da8ac3bfe387c2aecc148f16a10e74c9d3805bd
Dingzhe Li <cool4074@gmail.com>
2015-12-09 21:20:44 -0500

    picture server go back to old approach

--- Server/Picture/picture.js
+++ Server/Picture/picture.js
@@ -2,63 +2,27 @@ function listen(){
 	var port=9071;
 	var net=require('net');
 	var server=net.createServer(function(socket) {
-			var j = -1;
-			var json;
-			var picdata;
-			var picid;
-			console.log("enter createserver",j);
 			socket.on('data', function(data) {
-				console.log("socket on data");
-				console.log(data.length);
-				try{
-					if(j == -1){
-						json=JSON.parse(data.toString());
-						j = 0;
-					}
-					//if we get the signal of picture
-					if(j == 1){
-						picdata += data;
-					}
-					if(j == 0){
-						if(json.operation == "upload"){
-							console.log(json);
-							var createModule=require("./createnew");
-							picid = createModule.createnew(data, socket);
-							j = 1;
-						}else if(json.operation == "getpic"){
-							console.log("getpic");
-							var getModule=require("./getpic");
-							console.log(json.id);
-							getModule.getpic(json, socket);
-						}else{
-							socket.write(JSON.stringify({error:"Unknown Operation"}));
-							socket.destroy();
-						}
-					}
-				}catch(e){
-					console.error(e);
-					socket.write(JSON.stringify({error:e.toString()}));
-					socket.destroy();
-				}
-			});
-			socket.on("end",function(){
-				console.log("socket end");
-				console.log("pic id: ",picid);
-				console.log("global id: ",global.id);
-				if(j == 0){
-					j = -1;
-					picdata = null;
-					picid = null;
-					return;
-				}
-				j = -1;
-				if(picdata != null){
-					var uploadModule=require("./upload");
-					uploadModule.upload(picdata, socket,global.id);
-				}
-				picdata = null;
-				picid = null;
-			});
+            				try{
+            					var json=JSON.parse(data.toString());
+            					if (json.operation=="upload"){
+            						var postModule=require("./upload");
+            						postModule.upload(json, socket);
+            					}
+            					else if (json.operation=="getpic"){
+            						var deleteModule = require("./getpic");
+            						deleteModule.getpic(json,socket);
+            					}
+            					else{
+            						socket.write(JSON.stringify({error:"Unknown Operation"}));
+            						socket.destroy();
+            					}
+            				}catch(e){
+            					console.error(e);
+            					socket.write(JSON.stringify({error:e.toString()}));
+            					socket.destroy();
+            				}
+            			});
 		});
 		server.listen(port);
 }
--- Server/Picture/upload.js
+++ Server/Picture/upload.js
@@ -1,36 +1,18 @@
-exports.upload=function (receivedObj,socket,picid){
-	ObjectID = require('mongodb').ObjectID;
-	BSON = require('mongodb').BSONPure;
-	var ret = {};
-console.log("upload module");
+exports.upload=function (receivedObj,socket){
+	ret = {};
 	try{
-		if (receivedObj == null) {
-			ret.success = false;
-			ret.pictureId = -1;
-			ret.msg = "no data provided"
+		global.collection.insert({"data":receivedObj.data},function(err, docsInserted){
+			if(err){
+				ret.success=false;
+				ret.msg="error in insert";
+			}
+			else{
+				ret.success=true;
+				ret.id=docsInserted.ops[0]._id;
+			}
 			socket.write(JSON.stringify(ret));
 			socket.destroy();
-		}
-
-		var id = "";
-		id = picid;
-		var objectid = new ObjectID(id);
-		global.collection.update({"_id":objectid},{"_id":objectid,"data":receivedObj},
-												function (err, result) {
-													if (err){
-														ret.success = false;
-														ret.msg="error in update";
-														throw err;
-													}
-													else{
-														ret.success = true;
-														ret.msg="success";
-													}
-													console.log(result);
-													//socket.write(JSON.stringify(ret));
-													socket.destroy();
-												});
-
+		});
 	}catch(e){
 		console.error(e);
 		ret.error=e.toString();
